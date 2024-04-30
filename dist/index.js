33f34ff03216585b4650d632e3564c5983ddf990
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-05-01 03:08:33 +0600

    EXPLORE : Parameter's type checking in functions. And also Date constructor as a object type

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 const myName = "Abdullah";
 console.log(`My name is ${myName}`);
+const printGreeting = (pt) => {
+    console.log(`Hellow Mr: ${pt.name}. Today is ${pt.date.toLocaleDateString()}`);
+};
+printGreeting({ name: myName, date: new Date() });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 const myName : string = "Abdullah";
 
 console.log(`My name is ${myName}`);
+
+
+const printGreeting = (pt: {name: string; date: Date}) => {
+    console.log(`Hellow Mr: ${pt.name}. Today is ${pt.date.toLocaleDateString()}`)
+}
+
+printGreeting({name: myName, date: new Date()})
