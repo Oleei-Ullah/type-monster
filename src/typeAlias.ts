ebc867db69c424f68e6d4a7097da7f0d274be5aa
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-05-01 12:16:27 +0600

    (TYPEALIAS): Typealias is just awesome to work with in typescript

---
+++ src/typeAlias.ts
@@ -0,0 +1,19 @@
+type Person = {
+  name: string;
+  age: number;
+  isMarried: boolean;
+  lastName?: string;
+};
+
+const person1: Person = {
+  name: "abdullah",
+  age: 56,
+  isMarried: false,
+  lastName: "mahibullah",
+};
+
+const person2: Person = {
+  name: "Karimullah",
+  age: 56,
+  isMarried: false,
+};
