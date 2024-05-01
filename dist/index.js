f0761fabba45efa50514401b359e2192f593f852
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-05-01 10:33:16 +0600

    (ARROWFUNCTION): Type checking in arrow function

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 "use strict";
+//Normal function's type checking
 function sum(num1, num2) {
     console.log(`My balanec is ${num1 * num2}`);
     return `My balanec is ${num1 * num2}`;
 }
 sum(2, 3);
+//Arrow function's type checking
+const addArrow = (num1, num2 = 40) => num1 * num2;
+console.log(addArrow(3));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+//Normal function's type checking
 function sum(num1 : number, num2: number) : string {
     console.log(`My balanec is ${num1 * num2}`)
     return `My balanec is ${num1 * num2}`
 }
 
 sum(2,3)
+
+//Arrow function's type checking
+
+const addArrow = (num1: number, num2: number = 40) : number => num1 * num2;
+
+console.log(addArrow(3))
