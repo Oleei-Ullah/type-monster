091f65e513d1fb5e56db501f7554888f1df88cf8
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-05-01 09:56:27 +0600

    (LITERAL TYPE CHECKING): No other options without literal type checking if there just some limited value.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-const person : {name: string; age: number, married?: boolean} = {
-    name: "Karim Ullah",
-    age: 23,
-}
+const person: {
+  name: string;
+  fatherName: "Rahim Ullah"; // ---> Literal type checking.
+  age: number;
+  isMarried?: boolean;
+} = {
+  name: "Karim Ullah",
+  fatherName: "Rahim Ullah",
+  age: 23,
+  isMarried: true,
+};
